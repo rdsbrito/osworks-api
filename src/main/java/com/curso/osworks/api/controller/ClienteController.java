9d0a7a22f5058672d8ffa080e0c8360166d08b71
Rafael Brito <rafael.brito@fastalelo.onmicrosoft.com>
2020-08-20 01:21:13 -0300

    Aula 2 ate 44 minutos

---
+++ src/main/java/com/curso/osworks/api/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package com.curso.osworks.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.osworks.domain.model.Cliente;
+import com.curso.osworks.domain.repository.ClienteRepository;
+
+@RestController
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar(){
+		return clienteRepository.findAll();
+	}
+}
---
+++ src/main/java/com/curso/osworks/domain/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.curso.osworks.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.curso.osworks.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
